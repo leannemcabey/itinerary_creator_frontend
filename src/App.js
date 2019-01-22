7f48d4e9b62084366dd10ae929301c9be6b41d5e
leannemcabey <leanne.m.cabey@gmail.com>
2019-01-22 12:33:45 -0500

    Change price to be less than or equal to budget, create functionality for generating itinerary

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Itinerary from './containers/Itinerary';
 import Quiz from './components/Quiz';
--- src/containers/Itinerary.js
+++ src/containers/Itinerary.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
-import Place from '../components/Place'
+// import Place from '../components/Place'
 
 class Itinerary extends Component {
 
   filteredPlaces = () => {
-    //need to make it random
-    if (this.props.itinerary.city !== null){
-      const city = this.props.itinerary.city 
-      const state = this.props.itinerary.state
-      const budget = this.props.itinerary.budget
+    const city = this.props.itinerary.city
+    const state = this.props.itinerary.state
+    const budget = this.props.itinerary.budget
 
-      return this.props.places.filter(place => place.city === city && place.state === state && place.price_level === budget)
-    }
+    const availablePlaces = this.props.places.filter(place => place.city === city && place.state === state && place.price_level <= budget)
 
+    return availablePlaces
   }
 
+  generateItinerary = () => {
+    if (this.props.itinerary.city !== null) {
+      let availablePlaces = this.filteredPlaces()
+      const itineraryPlaces = []
+
+      for (let i = 0; i < 5; i++) {
+        let index = Math.floor(Math.random() * availablePlaces.length)
+        itineraryPlaces.push(availablePlaces[index])
+        availablePlaces = availablePlaces.filter(place => place !== availablePlaces[index])
+      }
+      return itineraryPlaces
+    }
+  }
 
   render() {
-    console.log(this.filteredPlaces())
+    console.log(this.generateItinerary())
+    // This is console-logging 3 times upon page load. This component must be rendering 3 times. Should it be?
     return (
       <div>
         <h1> Itinerary</h1>
-        
+
       </div>
     );
   }
